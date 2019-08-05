23ae6d672e9a73546aa4cd73b5c7d8a7e0232cd9
david.wang <david.wang@autoflight.com>
2019-08-05 14:02:56 +0800

    remove action

--- include/rtsp_receiver_component.h
+++ include/rtsp_receiver_component.h
@@ -6,8 +6,8 @@
 #include "sensor_msgs/msg/image.hpp"
 #include "std_srvs/srv/set_bool.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "perception_msgs/action/switch.hpp"
-#include "rclcpp_action/rclcpp_action.hpp"
+// #include "perception_msgs/action/switch.hpp"
+// #include "rclcpp_action/rclcpp_action.hpp"
 
 namespace ros2_videostreamer
 {
@@ -17,8 +17,8 @@ namespace ros2_videostreamer
         RtspReceiverNode();
         ~RtspReceiverNode();
 
-    using Switch = perception_msgs::action::Switch;
-    using GoalHandleSwitch = rclcpp_action::ServerGoalHandle<Switch>;
+    // using Switch = perception_msgs::action::Switch;
+    // using GoalHandleSwitch = rclcpp_action::ServerGoalHandle<Switch>;
 
     private:
 
@@ -31,11 +31,11 @@ namespace ros2_videostreamer
         void topic_rtsp_uri_callback_shared(const std_msgs::msg::String::SharedPtr msg);
         
         // action member functions
-        rclcpp_action::GoalResponse handle_goal (const rclcpp_action::GoalUUID & uuid, 
-            std::shared_ptr<const Switch::Goal> goal);
-        void handle_accepted(const std::shared_ptr<GoalHandleSwitch> goal_handle);
-        rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<GoalHandleSwitch> goal_handle);
-        void execute(const std::shared_ptr<GoalHandleSwitch> goal_handle);
+        // rclcpp_action::GoalResponse handle_goal (const rclcpp_action::GoalUUID & uuid, 
+        //     std::shared_ptr<const Switch::Goal> goal);
+        // void handle_accepted(const std::shared_ptr<GoalHandleSwitch> goal_handle);
+        // rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<GoalHandleSwitch> goal_handle);
+        // void execute(const std::shared_ptr<GoalHandleSwitch> goal_handle);
             
         void timer_check_alive_callback();
         void timer_stream_controller_callback();
@@ -69,7 +69,7 @@ namespace ros2_videostreamer
         rclcpp::Subscription<std_msgs::msg::String>::SharedPtr                      rtsp_uri_;  
         rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr                          switch_service_tracker_;
         rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr                          switch_service_detector_;
-        rclcpp_action::Server<Switch>::SharedPtr                                    action_server_;
+        // rclcpp_action::Server<Switch>::SharedPtr                                    action_server_;
     };
 
 }// namespace read_rtsp
